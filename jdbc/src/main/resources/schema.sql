b36fe62de7b5b770980f7ba361ef855c3e556787
Pa4aPV <Pa4aPV@yandex.ru>
2021-05-07 14:09:24 +0700

    seven

--- jdbc/src/main/resources/schema.sql
+++ jdbc/src/main/resources/schema.sql
@@ -1,12 +1,3 @@
-DROP TABLE IF EXISTS book;
-CREATE TABLE book
-(
-    id bigserial,
-    name varchar,
-    id_author bigint,
-    id_genre bigint
-);
-
 DROP TABLE IF EXISTS author;
 CREATE TABLE author
 (
@@ -19,4 +10,13 @@ CREATE TABLE genre
 (
     id bigserial,
     name varchar
+);
+
+DROP TABLE IF EXISTS book;
+CREATE TABLE book
+(
+    id bigserial,
+    name varchar,
+    id_author bigint REFERENCES author (id),
+    id_genre bigint REFERENCES genre (id)
 );
