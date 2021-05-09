8e6e24609975e06ac7d4073e208bba9cbcd50786
Pasha <pa4aPV@yandex.ry>
2021-05-09 09:43:52 +0700

    asd

--- jpa/src/main/java/ru/otus/spring/jpa/domain/Author.java
+++ jpa/src/main/java/ru/otus/spring/jpa/domain/Author.java
@@ -1,19 +1,24 @@
 package ru.otus.spring.jpa.domain;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "author")
 public class Author {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private Long id;
+	@Column(name = "name")
 	private String name;
 
 	public Author() {
