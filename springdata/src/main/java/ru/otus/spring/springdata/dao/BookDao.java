6f584743291f34fb133dbb81b5e2bb355d3c90a6
Pa4aPV <Pa4aPV@yandex.ru>
2021-05-28 13:30:53 +0700

    two n+1

--- springdata/src/main/java/ru/otus/spring/springdata/dao/BookDao.java
+++ springdata/src/main/java/ru/otus/spring/springdata/dao/BookDao.java
@@ -1,9 +1,14 @@
 package ru.otus.spring.springdata.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.otus.spring.springdata.domain.Book;
 
 
 
 public interface BookDao extends JpaRepository<Book, Long> {
+	@EntityGraph(attributePaths = {"author", "genre"})
+	List<Book> findAll();
 }
